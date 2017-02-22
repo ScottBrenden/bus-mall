22d81bbaf6791ce4a6eff15c61d1a1ff2b9b936f
Scott Brenden <scott.b.brenden@gmail.com>
2017-02-22 14:51:30 -0800

    local storage functional

--- app.js
+++ app.js
@@ -131,6 +131,10 @@ function printNumClicks() {
   }
 }
 //-----------------------------------------------A
+function toLocalStorage(products){
+  localStorage.products = JSON.stringify(products);
+  console.log('Saved to local storage');
+}
 
 var newVote = document.getElementById('img-selector');
 newVote.addEventListener('click', voteCast);
@@ -160,91 +164,11 @@ function voteCast(event){
   } else if (votes == 26){
     calcPercentages();
     printNumClicks();
-    newElement('canvas', 'id', 'click-chart', 'canvases', '');
-    newElement('canvas', 'id', 'percent-chart', 'canvases', '');
-    chartWorkPlease();
+    toLocalStorage(imgs);
+    newElement('a', 'href', 'charts.html', 'data-link', 'Graphs');
+    // newElement('canvas', 'id', 'click-chart', 'canvases', '');
+    // newElement('canvas', 'id', 'percent-chart', 'canvases', '');
+    // chartWorkPlease();
   }
 }
 //charts------------------------------------V
-function chartWorkPlease(){
-  var ctx = document.getElementById('click-chart').getContext('2d');
-  var pct = document.getElementById('percent-chart').getContext('2d');
-
-  var clickData = [];
-  var labelNames = [];
-  var percentageData = [];
-  var chartColors = [];
-
-  function pushChartArrs() {
-    for (var i = 0; i < imgs.length; i++) {
-      labelNames.push(imgs[i].name);
-      clickData.push(imgs[i].numClicked);
-      percentageData.push(imgs[i].percentage);
-    }
-  }
-  pushChartArrs();
-  // console.log(clickData);
-  // console.log(labelNames);
-
-//----------Click chart---------------------V
-  var clickChart = {
-    type: 'bar',
-    data: {
-      labels: labelNames,
-      datasets: [{
-        label: 'Number of Clicks',
-        data: clickData,
-        backgroundColor: 'white'
-      }],
-    },
-    options: {
-      legend: {labels:{fontColor:'#fff', fontSize: 18}},
-      scales: {
-        yAxes: [{
-          ticks: {
-            fontColor:'#fff',
-            beginAtZero:true
-          }
-        }],
-        xAxes:[{
-          ticks: {
-            fontColor:'#fff'
-          }
-        }]
-      }
-    }
-  };
-//------------------------------------------A
-
-//-------% chart---------------------V
-  var percChart = {
-    type: 'polarArea',
-    data: {
-      labels: labelNames,
-      datasets: [{
-        label: 'Percentage of Clicks',
-        data: percentageData,
-        backgroundColor: 'white'
-      }],
-    },
-    options: {
-      legend: {labels:{fontColor:'#fff'}},
-      scales: {
-        yAxes: [{
-          ticks: {
-            fontColor:'#fff',
-            beginAtZero:true
-          }
-        }],
-        xAxes:[{
-          ticks: {
-            fontColor:'#fff'
-          }
-        }]
-      }
-    }
-  };
-//-------------------------------------------A
-  var myChart = new Chart(ctx, clickChart);
-  var myChart = new Chart(pct, percChart);
-}
